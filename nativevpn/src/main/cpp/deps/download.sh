c86a8dd658d3dd884f7357dfb4a4ddaaa1f89140
Anton Valishin <anton@valishin.ru>
2024-04-27 23:23:34 +0300

    Update download.sh

--- nativevpn/src/main/cpp/deps/download.sh
+++ nativevpn/src/main/cpp/deps/download.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 export SOFTETHERVPN_VERSION=5.02.5181
 export OPENSSL_VERSION=3.2.1
-export LIBICONV_VERSION=1.17
 export SODIUM_VERSION=1.0.19-RELEASE
-#export NCURSES_VERSION_TAG=34cc24447dc9e5700110580c784d9606f6cff5f0
-#export READLINE_VERSION=8.1
+
 
 mkdir -r external; cd external;
 # Clone OpenSSL
@@ -14,17 +12,6 @@ else
     echo "Directory 'openssl' already exists, skipping clone."
 fi
 
-# Download and extract libiconv
-if [ ! -d "iconv" ]; then
-    wget https://ftp.gnu.org/pub/gnu/libiconv/libiconv-$LIBICONV_VERSION.tar.gz -O libiconv.tar.gz
-    mkdir -p iconv
-    tar -xzvf libiconv.tar.gz -C iconv
-    (cd iconv && mv libiconv*/* .)
-    rm -f libiconv.tar.gz
-else
-    echo "Directory 'iconv' already exists, skipping extract."
-fi
-
 
 # Clone libsodium
 if [ ! -d "libsodium" ]; then
@@ -32,26 +19,6 @@ if [ ! -d "libsodium" ]; then
 else
     echo "Directory 'libsodium' already exists, skipping clone."
 fi
-
-# Clone ncurses
-#if [ ! -d "ncurses" ]; then
-#    git clone https://android.googlesource.com/platform/external/ncurses
-#    (cd ncurses && git checkout $NCURSES_VERSION_TAG)
-#else
-#    echo "Directory 'ncurses' already exists, skipping clone."
-#fi
-
-# Download and extract readline
-#if [ ! -d "readline" ]; then
-#    wget https://ftp.gnu.org/gnu/readline/readline-$READLINE_VERSION.tar.gz -O readline.tar.gz
-#    mkdir -p readline
-#    tar -xzvf readline.tar.gz -C readline
-#    (cd readline && mv readline*/* .)
-#    rm -f readline.tar.gz
-#else
-#    echo "Directory 'readline' already exists, skipping extract."
-#fi
-#rm -f readline.tar.gz
 
 # Clone SoftEtherVPN
 if [ ! -d "SoftEtherVPN" ]; then
